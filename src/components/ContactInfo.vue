e29c3d50680a5eac7737b3be44ead8b19325ba07
Musa Musa <mussamustapha0306@gmail.com>
2024-10-26 11:35:57 +0300

    changes

---
+++ src/components/ContactInfo.vue
@@ -0,0 +1,77 @@
+<script setup>
+import { defineComponent, reactive } from 'vue';
+
+defineComponent({
+    name: 'ContactInfo',
+});
+
+defineProps({
+    header: String,
+});
+
+const data = reactive([
+    {
+        icon: 'mdi-cellphone-sound',
+        title: 'Phone',
+        value: '+ 255 742 944 121',
+        href: 'tel:+380638720990',
+        target: null,
+    },
+    {
+        icon: 'mdi-message-text',
+        title: 'Telegram',
+        value: '@mussa',
+        href: '',
+        target: '_blank',
+    },
+    {
+        icon: 'mdi-gmail',
+        title: 'Gmail',
+        value: 'mussamustapha0306@gmail.com',
+        href: 'mailto:mussamustapha0306@gmail.com',
+        target: null,
+    },
+    {
+        icon: 'mdi-github',
+        title: 'GitHub',
+        value: 'dembele360',
+        href: 'https://github.com/dembele360',
+        target: '_blank',
+    },
+    {
+        icon: 'mdi-linkedin',
+        title: 'LinkedIn',
+        value: 'mussa.mustapha',
+        href: 'https://www.linkedin.com/in/mussa.mustapha/',
+        target: '_blank',
+    },
+]);
+</script>
+
+<template>
+    <div class="contact-info">
+        <h5 class="text-h5 text-uppercase text-deep-purple-accent-1">{{ header }}</h5>
+        <div class="contact-info__item mt-3" v-for="(item, i) in data" :key="i">
+            <div class="contact-info__icon">
+                <v-icon size="x-large">{{ item.icon }}</v-icon>
+            </div>
+            <div class="contact-info__content">
+                <span class="text-uppercase text-button">{{ item.title }}</span>
+                <a :href="item.href" :target="item.target">{{ item.value }}</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.contact-info__item {
+    display: flex;
+    align-items: flex-end;
+}
+
+.contact-info__content {
+    display: flex;
+    flex-direction: column;
+    margin-left: 10px;
+}
+</style>
---
+++ src/components/Timelines.vue
@@ -0,0 +1,81 @@
+<script setup>
+import { defineComponent, reactive } from 'vue';
+
+defineComponent({
+    name: 'Timelines',
+});
+
+const timelines = reactive([
+
+
+    {
+        id: 0,
+        icon: 'mdi-briefcase-variant',
+        color: 'blue-darken-3',
+        title: 'Fabonic Ltd',
+        position: 'Software Engineer',
+        period: 'DEC 2021 — NOWADAYS',
+        description: 'Working on various software development projects.'
+    },
+    {
+        id: 1,
+        icon: 'mdi-book-education',
+        color: 'blue-darken-3',
+        title: 'Mzumbe University (MU)',
+        position: 'Bachelor of Science in Information and Communication Technology with Business',
+        period: '2018 - 2021',
+        description: 'Pursued a degree focused on ICT and business applications.'
+    },
+    {
+        id: 2,
+        icon: 'mdi-book-education',
+        color: 'amber darken-1',
+        title: 'Nainokanoka Secondary School',
+        position: 'Advanced Certificate in Secondary Education',
+        period: '2016 - 2018',
+        description: 'Completed advanced secondary education.'
+    },
+    {
+        id: 3,
+        icon: 'mdi-book-education',
+        color: 'cyan lighten-1',
+        title: 'Lemara Secondary School',
+        position: 'Certificate of Secondary Education (CSEE)',
+        period: '2012 – 2015',
+        description: 'Completed secondary education.'
+    },
+    {
+        id: 4,
+        icon: 'mdi-book-education',
+        color: 'cyan lighten-1',
+        title: 'Lemara Primary School',
+        position: 'Certificate in Primary Education',
+        period: '2005 – 2011',
+        description: 'Completed primary education.'
+    },
+
+]);
+
+</script>
+
+<template>
+    <div class="timeline mt-5">
+        <h5 class="text-h5 text-uppercase text-center text-deep-purple-accent-1">Education & work</h5>
+        <v-timeline class="mt-5">
+            <v-timeline-item v-for="timeline in timelines" :key="timeline.id" :dot-color="timeline.color"
+                :icon="timeline.icon" size="large">
+                <template v-slot:opposite>
+                    <div :class="`pt-1 headline font-weight-bold text-button text-${timeline.color}`"
+                        v-text="timeline.period"></div>
+                </template>
+                <div>
+                    <h2 :class="`mt-n1 headline font-weight-light mb-4 text-${timeline.color}`">
+                        {{ timeline.title }}
+                    </h2>
+                    <h5 :class="`text-button text-${timeline.color}`">{{ timeline.position }}</h5>
+                    <span class="text-subtitle-2">{{ timeline.description }}</span>
+                </div>
+            </v-timeline-item>
+        </v-timeline>
+    </div>
+</template>
